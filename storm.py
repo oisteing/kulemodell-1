6a37195fe39ade82e8e36baf0109bb7c8eb57d6d
oisteing <66906363+oisteing@users.noreply.github.com>
2025-04-01 16:27:26 +0200

    Update storm.py

--- storm.py
+++ storm.py
@@ -2,7 +2,7 @@ import streamlit as st
 from random import *
 from statistics import *
 
-# Dictionary for translations
+# Ordliste for omsetjing
 translations = {
     "Nynorsk": {
         "go": "Køyr",
@@ -21,7 +21,7 @@ translations = {
         "totalt": "Totalt",
         "modell1": "Denne modellen lar deg legge eit antal raude og blå kuler i ei eske, for så å trekkje ut eit visst antal. Dette kan gjentakast fleire gonger med fleire simuleringar.",
         "modell2": "Denne modellen lar deg trekkje kule etter kule frå eska til du treffer på den første raude kula.",
-        "modell3": "Denne modellen lar deg legge eit antal raude og blå kuler i ei eske, for så å trekkje ut eit visst antal. Dette kan gjentakast fleire gonger med fleire simuleringar.",
+        "modell3": "Denne modellen lar deg legge eit antal raude og blå kuler i ei eske, for så å trekkje ut ei og ei kule til du har trekt ut alle dei raude.",
         "antall gongar du trekte": "Antal gongar du trekte", 
         "kor mange kuler i eska": "Kor mange kuler er det i eska? (1-10000)",
         "kor mange raude i eska": "Kor mange raude kuler skal det vere i eska?",
@@ -32,7 +32,7 @@ translations = {
         "antall raude": "Antal raude",
         "antall gongar": "Antal gongar",
         "du måtte trekkje": "Du måtte trekkje",
-        "gonger før første": "gongar før du fekk den første raude kula",
+        "gongar før første": "gongar før du fekk den første raude kula",
         "gongar før alle": "gongar for å trekkje alle dei"
     },
     "Bokmål": {
@@ -52,7 +52,7 @@ translations = {
         "totalt": "Totalt",
         "modell1": "Denne modellen lar deg legge et antall røde og blå kuler i ei eske, for så å trekke ut et visst antall. Dette kan gjentas flere ganger med flere simuleringer.",
         "modell2": "Denne modellen lar deg trekke kule etter kule fra eska til du treffer på den første røde kula.",
-        "modell3": "Denne modellen lar deg legge røde og blå kuler i ei eske, for så å trekke ut et visst antall. Dette kan gjentas flere ganger med flere simuleringer.",
+        "modell3": "Denne modellen lar deg legge røde og blå kuler i ei eske, for så å trekke ut ei og ei kule til du har trukket alle de røde.",
         "antall gongar du trekte": "Antall ganger du trakk ", 
         "kor mange kuler i eska": "Hvor mange kuler skal det være i eska? (1-10000)",
         "kor mange raude i eska": "Hvor mange røde kuler er det i eska?",
@@ -102,9 +102,7 @@ translations = {
 options = ["Nynorsk", "Bokmål", "English"]
 selection = st.sidebar.radio(" ", options)
 #st.sidebar.write("(c) oisteing")
-
 st.title(translations[selection]['tittel'])
-
 st.sidebar.write(translations[selection]['intro'])
 #st.sidebar.write("Sjekk kombinatorikk-appen og:")
 #st.sidebar.page_link("https://ezudqix98qb6m3zykxcxug.streamlit.app/", label="Kombinatorikk",)
@@ -136,7 +134,7 @@ def skriv_regnskap(lst, modell, numsim):
     st.write(translations[selection]['standardavvik'],f": {std: .2f}")
   st.text("")
   if modell==1:
-    if antall_som_trekkes<50: #Skriver ut kor mange gonger dei forskjellige verdiane dukket opp, dersom det er mindre enn 50 kuler
+    if antall_som_trekkes<50: #Skriv ut kor mange gongar dei forskjellige verdiane dukket opp, dersom det er færre enn 50 kuler
      for k in range(antall_som_trekkes+1):
        st.write(translations[selection]['antall gongar du trekte'], k, translations[selection]['raude'], " : ", sum(1 for i in lst if i  == k),"  ")
   
@@ -170,7 +168,7 @@ with tab1:
   st.text(translations[selection]['modell1'])
   
   kuler_i_esken = int(st.number_input(translations[selection]['kor mange kuler i eska'], min_value=1, max_value=10000, value=6))
-  raude_i_esken = int(st.number_input(translations[selection]['kor mange raude i eska'], min_value=1, value=1, max_value=kuler_i_esken))
+  raude_i_esken = int(st.number_input(translations[selection]['kor mange raude i eska']+" (1-"+f"{kuler_i_esken})", min_value=1, value=1, max_value=kuler_i_esken))
   antall_som_trekkes = int(st.number_input(translations[selection]['trekkantall'], min_value=1, value=6))
   tilbakelegging=st.checkbox(translations[selection]['legg tilbake'], key=11, value=True)
   antall_simuleringar = int(st.number_input(translations[selection]['antall simuleringar'], min_value=1, max_value=10000, value=10))
@@ -178,7 +176,8 @@ with tab1:
 
   if st.button(translations[selection]['go'], key=1):
       if not tilbakelegging and antall_som_trekkes>kuler_i_esken:
-        st.write(translations[selection]['ikkje legg tilbake'])
+        st.warning(translations[selection]['ikkje legg tilbake'], icon="⚠️")
+        #st.write(translations[selection]['ikkje legg tilbake'])
       else:      
         for j in range(antall_simuleringar):
           resultatstreng=" "
@@ -190,7 +189,7 @@ with tab1:
           raude=0
           blaa=0
           trekk(antall_som_trekkes)
-          if antall_simuleringar<11:
+          if antall_simuleringar<11 and antall_som_trekkes<500:
             st.markdown(resultatstreng)
             st.write(translations[selection]['totalt'], raude, translations[selection]['raude og'], blaa, translations[selection]['blå']+".")
           resultater.append(raude)
@@ -221,7 +220,7 @@ with tab2:
         return False
   st.text(translations[selection]['modell2'])
   kuler_i_esken2 = int(st.number_input(translations[selection]['kor mange kuler i eska'], min_value=1, value=100, key=21))
-  raude_i_esken2 = int(st.number_input(translations[selection]['kor mange raude i eska'] ,min_value=1, max_value=kuler_i_esken2, key=22))
+  raude_i_esken2 = int(st.number_input(translations[selection]['kor mange raude i eska']+" (1-"+f"{kuler_i_esken2})" ,min_value=1, max_value=kuler_i_esken2, key=22))
   tilbakelegging2=st.checkbox(translations[selection]['legg tilbake'], key=5, value=False)
   antall_simuleringar2 = int(st.number_input(translations[selection]['antall simuleringar'], min_value=1, max_value=10000, value=2, key=23))
   resultater2=[]
@@ -268,8 +267,8 @@ with tab3:
       eske.pop(kulenr-1)
       return False
 
-  kuler_i_esken3 = int(st.number_input(translations[selection]['kor mange kuler i eska'], min_value=1, value=100, key=31))
-  raude_i_esken3 = int(st.number_input(translations[selection]['kor mange raude i eska']+f"(max {kuler_i_esken3})",min_value=1, value=5, key=32))
+  kuler_i_esken3 = int(st.number_input(translations[selection]['kor mange kuler i eska'], min_value=1, max_value=10000, value=100, key=31))
+  raude_i_esken3 = int(st.number_input(translations[selection]['kor mange raude i eska']+f" (max {kuler_i_esken3})",min_value=1, max_value=kuler_i_esken3, value=1, key=32))
   antall_simuleringar3 = int(st.number_input(translations[selection]['antall simuleringar'], key=33, min_value=1, value=2, max_value=10000))
   resultater3=[]
 
@@ -303,3 +302,10 @@ with tab3:
         st.write(translations[selection]['du måtte trekkje']+f" {antall_trekk} "+translations[selection]['gongar før alle']+f" {raude_i_esken3} "+translations[selection]['raude']+".") 
     
     skriv_regnskap(resultater3, 3, antall_simuleringar3)
+    
+    val=[]
+    for k in range(max(resultater3)+1):
+        a = sum(1 for i in resultater3 if i  == k) 
+        val.append(a)
+    
+    st.bar_chart(val, x_label=translations[selection]['antall gongar du trekte'], y_label=translations[selection]['antall gongar'], )
